dd8005bc58b5dbade5fd4f2b5796f01f3563db9e
Deivid Lucas Marques <21520180082@maristabrasil.g12.br>
2025-11-04 08:53:13 -0300

    seleciona elemento da pagina, link, dropdown e nav-link

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const link = document.querySelector(".link");
+    const dropdown = document.querySelector(".dropdown-vagas");
+    const navLink = link.querySelector(".nav-link");
 
     console.log("DOM carregado!");
 });
