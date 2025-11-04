040974445b346f2de49e2b11c18a1e26df3de2d3
luczzy <drividlucas10@gmail.com>
2025-11-04 15:26:57 -0300

    adiciona logica de abrir e fechar o login-modal

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -23,4 +23,22 @@ document.addEventListener("DOMContentLoaded", () => {
   dropdown.addEventListener("mouseenter", showDropdown);
   dropdown.addEventListener("mouseleave", hideDropdown);
 
+  // modal de login
+  const modal = document.getElementById("login-modal");
+  const btnEntrar = document.querySelector(".btn-entrar");
+  const closeBtn = document.querySelector(".close-modal");
+
+  if (btnEntrar && modal && closeBtn) {
+    // abre o modal
+    btnEntrar.addEventListener("click", () => {
+      modal.style.display = "flex";
+      document.body.style.overflow = "hidden";
     });
+
+    // fecha no "x"
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+      document.body.style.overflow = "auto";
+    });
+      }
+  });
