d7e8d79525f6e7d251945e7e2239c11772b5a90a
luczzy <drividlucas10@gmail.com>
2025-11-04 15:15:25 -0300

    adiciona estrutura de 'login-modal' no arquivo html

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -1,27 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const link = document.querySelector(".link");
-    const dropdown = document.querySelector(".dropdown-vagas");
-    const navLink = link.querySelector(".nav-link");
+  const link = document.querySelector(".link");
+  const dropdown = document.querySelector(".dropdown-vagas");
+  const navLink = link.querySelector(".nav-link");
 
-    let hoverTimeout;a
+  let hoverTimeout;
 
-    function showDropdown() {
-      clearTimeout(hoverTimeout);
-      dropdown.style.display = "block";
-      navLink.classList.add("active");
-    }
-  
-    function hideDropdown() {
-      hoverTimeout = setTimeout(() => {
-        dropdown.style.display = "none";
-        navLink.classList.remove("active");
-      }, 150);
-    }
+  function showDropdown() {
+    clearTimeout(hoverTimeout);
+    dropdown.style.display = "block";
+    navLink.classList.add("active");
+  }
 
-    link.addEventListener("mouseenter", showDropdown);
-    link.addEventListener("mouseleave", hideDropdown);
-    dropdown.addEventListener("mouseenter", showDropdown);
-    dropdown.addEventListener("mouseleave", hideDropdown);  
+  function hideDropdown() {
+    hoverTimeout = setTimeout(() => {
+      dropdown.style.display = "none";
+      navLink.classList.remove("active");
+    }, 150);
+  }
 
-    console.log("DOM carregado!");
-});
+  link.addEventListener("mouseenter", showDropdown);
+  link.addEventListener("mouseleave", hideDropdown);
+  dropdown.addEventListener("mouseenter", showDropdown);
+  dropdown.addEventListener("mouseleave", hideDropdown);
+
+    });
