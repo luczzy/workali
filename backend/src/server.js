fbd02a68ef47912e8c1ccac7e529c08c701bea66
luczzy <drividlucas10@gmail.com>
2025-11-06 19:55:51 -0300

    conecta rotas de autenticação ao servidor e testa endpoint de registro

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./Config/db.js";
-import authRoutes from "./Routes/authRoutes.js";
+import authRoutes from "./Routes/authRoutes.js"; 
 
 dotenv.config();
 
