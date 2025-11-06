a513459d8cd3ecfda161d004983c0d366d668c6a
luczzy <drividlucas10@gmail.com>
2025-11-06 20:03:57 -0300

    login funcional e geração de token JWT

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,8 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config(); 
+import express from "express";
 import { connectDB } from "./Config/db.js";
 import authRoutes from "./Routes/authRoutes.js"; 
-
-dotenv.config();
 
 // conecta ao MongoDB
 connectDB();
