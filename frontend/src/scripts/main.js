8c97e8d180c52079e39d241d8b377d1757bdddb7
luczzy <drividlucas10@gmail.com>
2025-11-11 18:44:17 -0300

    adiciona função no main.js para abrir o modal em outros botões e link

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -23,25 +23,29 @@ document.addEventListener("DOMContentLoaded", () => {
   dropdown.addEventListener("mouseenter", showDropdown);
   dropdown.addEventListener("mouseleave", hideDropdown);
 
-  // modal de login
   const modal = document.getElementById("login-modal");
-  const btnEntrar = document.querySelector(".btn-entrar");
   const closeBtn = document.querySelector(".close-modal");
+  const abrirModalElems = document.querySelectorAll(".abrir-modal");
 
-  if (btnEntrar && modal && closeBtn) {
-    // abre o modal
-    btnEntrar.addEventListener("click", () => {
-      modal.style.display = "flex";
-      document.body.style.overflow = "hidden";
+  if (modal && closeBtn) {
+    abrirModalElems.forEach((elem) => {
+      elem.addEventListener("click", (e) => {
+        e.preventDefault(); // evita que o link navegue
+        modal.style.display = "flex";
+        document.body.style.overflow = "hidden";
+      });
     });
 
-    // fecha no "x"
     closeBtn.addEventListener("click", () => {
       modal.style.display = "none";
       document.body.style.overflow = "auto";
     });
+
+    window.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto";
       }
-  });
-
-// Personalização para o menu mobile 
-
+    });
+  }
+});
